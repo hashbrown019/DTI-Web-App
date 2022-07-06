69c52ba9b2b138ec4355ac305ad798b40d934018
hashbrown019 <45794351+hashbrown019@users.noreply.github.com>
2022-07-06 10:50:00 +0800

    Changed from mysql to Sqlite

--- controllers/api.py
+++ controllers/api.py
@@ -10,8 +10,8 @@ app = Blueprint("api",__name__)
 cors = CORS(app)
 
 # rapid = sqlite
-# rapid= sqlite(c.SQLITE_DB)
-rapid = mysql(c.LOCAL_HOST,c.LOCAL_USER,c.LOCAL_PASSWORD,c.LOCAL_DATABASE)
+rapid= sqlite(c.SQLITE_DB)
+# rapid = mysql(c.LOCAL_HOST,c.LOCAL_USER,c.LOCAL_PASSWORD,c.LOCAL_DATABASE)
 
 farm_details = None
 
