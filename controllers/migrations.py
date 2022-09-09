fd01045c9dc0c8119c84929bfabd561f48f37d00
hashbrown019 <45794351+hashbrown019@users.noreply.github.com>
2022-09-09 17:24:17 +0800

    Upload EXCEL Feature

--- controllers/migrations.py
+++ controllers/migrations.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, session, redirect, jsonify, send_file
 from flask_session import Session
+# from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 from modules.Connections import mysql,sqlite
 import Configurations as c
 import os
@@ -7,6 +9,7 @@ import json
 from flask_cors import CORS,cross_origin
 import base64
 import sys
+import pandas as pd
 
 app = Blueprint("migrations",__name__)
 cors = CORS(app)
@@ -44,4 +47,70 @@ class _main:
 		
 		return jsonify(res)
 
+	@app.route("/excel_popu",methods=["POST","GET"])
+	def excel_popu():
+		file_name =  c.RECORDS+"/spreadsheets/vc_a_1.0.0_1662604608.xlsx" # path to file + file name
+		sheet =  "VC FORM A" # sheet name or sheet number or list of sheet numbers and names
 
+		df = pd.read_excel(io=file_name, sheet_name=sheet)
+		EXCEL_DATA = df.iterrows()
+
+		_result = {}
+		LLL = dict(EXCEL_DATA)
+		for key in LLL:
+			_result[key] = [] 
+			for val in LLL[key]:
+				_result[key].append(val)
+
+		counter = 0
+		new_struc = {}
+		__new_struc = []
+
+		t1 = None
+		t2 = None
+		t3 = None
+		# for colNames in df.head():
+		# 	# print(colNames.find("Unnamed"))
+		# 	if(colNames.find("Unnamed") != 0):
+		# 		new_struc[colNames] = {}
+		# 		t1 =colNames
+
+		# 	if (str(_result[0][counter]).lower().find("nan")):
+		# 		new_struc[t1][_result[0][counter]] = {}
+		# 		t2 = _result[0][counter]
+
+		# 	new_struc[t1][t2][_result[1][counter] ]= _main.x_filed_get(_result,counter)
+		# 	counter = counter + 1
+		# return jsonify(new_struc)
+
+
+		return jsonify(_result[1])
+	
+	def x_filed_get(_result,counter):
+		row_counter = 0
+		data = []
+		for _res in _result:
+			if row_counter >2 :
+				data.append(_result[_res][counter])
+			row_counter =  row_counter + 1
+		return []
+		# return data
+	
+
+	@app.route("/excel_upload",methods=["POST","GET"])
+	def excel_upload():
+		from datetime import date
+
+		today = str(date.today())
+		uploader = request.form['uploader']
+		excel_ = request.files
+		for excel in excel_:
+			f = excel_[excel]
+			f.save(os.path.join(c.RECORDS+"/spreadsheets/", uploader+"#"+today+"#"+secure_filename(f.filename)))
+		return _main.get_uploaded_excel()
+		# return data
+
+	@app.route("/get_uploaded_excel",methods=["POST","GET"])
+	def get_uploaded_excel():
+		return jsonify(os.listdir(c.RECORDS+"/spreadsheets/"))
+		# return data
