5665e688f755557462d855a3e0d1d8b90d56a2ef
hashbrown019 <45794351+hashbrown019@users.noreply.github.com>
2022-11-24 11:32:14 +0800

    Update apiV2.py

--- controllers/apiV2.py
+++ controllers/apiV2.py
@@ -208,7 +208,9 @@ class _main:
 				prof_1 = json.loads(json.loads(strsd)); 
 				if(f_id not in res_ls):res_ls[f_id] = {}
 				for key in prof_1:
-					if(key == 'farmer_img_base64'):prof_1[key]= "HIDDEN"
+					if(key == 'farmer_img_base64'):prof_1[key]= "HIDDEN";
+					if(key == 'post_harv-photo'):prof_1[key]= "HIDDEN";
+					if(key == 'farm-photo'):prof_1[key]= "HIDDEN";
 					res_ls[f_id]["{}__{}".format(prefix,key)] = prof_1[key]
 
 
