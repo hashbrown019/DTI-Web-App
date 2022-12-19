a70f6dcc74e50dc38e3c9cd5c2c0eccfacf13df3
hashbrown019 <45794351+hashbrown019@users.noreply.github.com>
2022-12-19 16:10:39 +0800

    Update Backend.py

--- Backend.py
+++ Backend.py
@@ -164,7 +164,7 @@ def excel_popu():
 						# FROM_EXCEL_RPOFILES = FROM_EXCEL_RPOFILES + append_data_excel_mdata(_result,path)
 					except Exception as e:
 						print(" * Error in [{}] :: {}".format(path,e))
-						# continue
+						continue
 
 
 			f = open(c.RECORDS+"/profiles/farmer_profile_EXCEL_LIST.json", "r")
@@ -175,6 +175,7 @@ def excel_popu():
 			f = open(c.RECORDS+"/profiles/farmer_profile_EXCEL_LIST.json", "w")
 			f.write(json.dumps(sdf))
 			f.close()
+			
 		else:
 			loads_.desc = "Skipping || {}".format(path)
 			print("Skipping || {}".format(path))
