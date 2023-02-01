0fa272667360ede478f918285580631f3ba79e52
cjmontanez1997 <117901200+cjmontanez1997@users.noreply.github.com>
2023-02-01 14:50:19 +0800

    Responsive NavBar

--- static/webrepstatic/js/index.js
+++ static/webrepstatic/js/index.js
@@ -34,3 +34,15 @@ function mini_carousel(ind){
 	// showDivs(slideIndex += ind);
 }
 
+
+
+// Navbar Dropdown
+
+$(document).ready(function(){
+  $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+    event.preventDefault(); 
+    event.stopPropagation(); 
+    $(this).parent().siblings().removeClass('open');
+    $(this).parent().toggleClass('open');
+  });
+});
